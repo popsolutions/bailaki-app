288775bf274640f0519b1bf73225476b27cdf789
rullyalves <rullyalvesz@gmail.com>
2021-03-16 08:42:45 -0300

    replace channel facade to channel service

--- lib/app/pages/match/match_controller.dart
+++ lib/app/pages/match/match_controller.dart
@@ -1,17 +1,19 @@
 import 'package:mobx/mobx.dart';
 import 'package:odoo_client/app/data/models/channel.dart';
-import 'package:odoo_client/app/data/services/channel_facade.dart';
+import 'package:odoo_client/app/data/models/channel_request_dto.dart';
+import 'package:odoo_client/app/data/services/channel_service.dart';
 part 'match_controller.g.dart';
 
 class MatchController = _MatchControllerBase with _$MatchController;
 
 abstract class _MatchControllerBase with Store {
-  final ChannelFacade _channelFacade;
+  final ChannelService _channelService;
   int _currentPartnerId;
 
-  set currentPartnerId(int currentPartnerId) => _currentPartnerId = currentPartnerId;
+  set currentPartnerId(int currentPartnerId) =>
+      _currentPartnerId = currentPartnerId;
 
-  _MatchControllerBase(this._channelFacade);
+  _MatchControllerBase(this._channelService);
 
   @observable
   ObservableFuture<List<Channel>> _matchesRequest =
@@ -21,8 +23,8 @@ abstract class _MatchControllerBase with Store {
 
   @action
   void load() {
-    _matchesRequest =
-        _channelFacade.findByPartner(_currentPartnerId).asObservable();
-        
+    _matchesRequest = _channelService
+        .findByPartner(ChannelRequestDto(currentPartnerId: _currentPartnerId))
+        .asObservable();
   }
 }
