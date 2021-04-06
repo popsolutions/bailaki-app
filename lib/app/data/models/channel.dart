3f64739846236f1ef83897bb3e282d05329d4b21
rullyalves <rullyalvesz@gmail.com>
2021-04-05 23:55:25 -0300

    create partner channel models and change inverse chatter name

--- lib/app/data/models/channel.dart
+++ lib/app/data/models/channel.dart
@@ -6,29 +6,53 @@ class Channel {
   final List<Message> messages;
   final int leftPartnerId;
   final int rightPartnerId;
-  Channel(
-      {this.channelId,
-      this.name,
-      this.messages,
-      this.leftPartnerId,
-      this.rightPartnerId});
+  final List<PartnerChannel> partners;
+
+  Channel({
+    this.channelId,
+    this.name,
+    this.messages,
+    this.leftPartnerId,
+    this.rightPartnerId,
+    this.partners,
+  });
 
   String get lastMessage => messages.isNotEmpty ? messages.last.body : '';
 
   String chatterName(int partnerId) {
-    final partnerNames = name.split(',');
-    return leftPartnerId == partnerId ? partnerNames.first : partnerNames.last;
+    return partners
+        .firstWhere((element) => element.id != partnerId, orElse: () => null)
+        ?.name;
   }
 
   factory Channel.fromJson(Map<String, dynamic> json) {
     return Channel(
+      partners: json['partners']
+          .map<PartnerChannel>((e) => PartnerChannel.fromJson(e))
+          .toList(),
       name: json['name'],
       channelId: json['id'],
       messages: json['messages']
           ?.map((e) => Message.fromJson(json['messages']))
           ?.toList(),
-      leftPartnerId: json['left_partner_id'],
-      rightPartnerId: json['right_partner_id'],
+      leftPartnerId: json['channel_partner_ids'].isNotEmpty
+          ? json['channel_partner_ids'].first
+          : null,
+      rightPartnerId: json['channel_partner_ids'].isNotEmpty
+          ? json['channel_partner_ids'].last
+          : null,
     );
+  }
+}
+
+class PartnerChannel {
+  int id;
+  String name;
+
+  PartnerChannel({this.id, this.name});
+
+  PartnerChannel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
   }
 }
