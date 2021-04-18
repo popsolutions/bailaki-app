a604e150b688a533bcdfa8f172bff7f5efb57d9e
rullyalves <rullyalvesz@gmail.com>
2021-04-17 23:37:09 -0300

    change partner service interface

--- lib/app/data/services/partner_service.dart
+++ lib/app/data/services/partner_service.dart
@@ -1,7 +1,7 @@
 import 'package:odoo_client/app/data/pojo/partner_detail.dart';
 import 'package:odoo_client/app/data/pojo/partners.dart';
 
-abstract class PartnerService{
-  Future<List<Partner>> finAll();
+abstract class PartnerService {
+  Future<List<Partner>> finAll(List<int> friendIds);
   Future<PartnerDetail> finById(int id);
 }
