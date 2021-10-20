f585e2b74cdd6d3f9af34b9059733616d20eea1e
Ismael <http.ismaeel@gmail.com>
2021-10-20 12:05:38 -0300

    ajuste no layout mensagem recebiba no chat_tile

--- lib/app/pages/match/components/chat_tile.dart
+++ lib/app/pages/match/components/chat_tile.dart
@@ -9,76 +9,94 @@ class ChatTile extends StatelessWidget {
   final String description;
   final Uint8List imageBytes;
   final int amount_newmessages;
-  const ChatTile(
-      {Key key,
-      this.padding,
-      this.onTap,
-      this.name,
-      this.description,
-      this.imageBytes,
-      this.amount_newmessages})
-      : super(key: key);
+  const ChatTile({
+    Key key,
+    this.padding,
+    this.onTap,
+    this.name,
+    this.description,
+    this.imageBytes,
+    this.amount_newmessages,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        padding: padding,
-        child: Row(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              //backgroundColor: Colors.blue,
-              backgroundImage:
-                  imageBytes != null ? MemoryImage(imageBytes) : null,
-            ),
-            const SizedBox(width: 20),
-            Flexible(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    name ?? '',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 23,
-                    ),
-                  ),
-                  Row(
+    return Stack(
+      children: [
+        InkWell(
+          onTap: onTap,
+          child: Container(
+            padding: padding,
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  //backgroundColor: Colors.blue,
+                  backgroundImage:
+                      imageBytes != null ? MemoryImage(imageBytes) : null,
+                ),
+                const SizedBox(width: 20),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Icon(
-                        Icons.subdirectory_arrow_left_sharp,
-                        color: Colors.grey[800],
-                        size: 15,
-                      ),
-                      const SizedBox(width: 3),
-                      Expanded(
-                        child: Text(
-                          description,
-                          style: TextStyle(
-                            color: Colors.grey[800],
-                            fontWeight: FontWeight.w300,
-                            fontSize: 15,
-                          ),
+                      Text(
+                        name ?? '',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 23,
                         ),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.subdirectory_arrow_left_sharp,
+                            color: Colors.grey[800],
+                            size: 15,
+                          ),
+                          const SizedBox(width: 3),
+                          Expanded(
+                            child: Text(
+                              description,
+                              style: TextStyle(
+                                color: Colors.grey[800],
+                                fontWeight: FontWeight.w300,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  if (amount_newmessages > 0)
-                    Text(
-                      amount_newmessages.toString(),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                        fontSize: 15,
-                      ),
-                    )
-                ],
-              ),
-            )
-          ],
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          bottom: 20,
+          left: 90,
+          child: Visibility(
+            visible: amount_newmessages > 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Text(
+                amount_newmessages.toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
